c1ddf237651ea9180e35cac3657280c668e9e2ef
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-01-03 14:43:05 -0300

    exer1

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let logado = true
 let lista = ['Joao', 19]
 let obj = {nome: 'joão', idade: 19 }
 console.log(typeof nome) // TIPO
+
+//EXERCICIO 1
+let carro = 'Ferrari'
+console.log(carro)
+
+let precoBolo = 19.99
+console.log(precoBolo)
+
+let cidade = 'São Paulo'
