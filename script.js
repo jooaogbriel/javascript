22fe35d195c085de8460f6d9b54aa49797861186
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-01-03 16:40:10 -0300

    condicionais

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-let nome = 'Joao'
-let idade = 19
-let logado = true
-let lista = ['Joao', 19]
-let obj = {nome: 'joão', idade: 19 }
-console.log(typeof nome) // TIPO
+let nome = 'Joao';
+let idade = 19;
+let logado = true;
+let lista = ['Joao', 19];
+let obj = {nome: 'joão', idade: 19 };
+console.log(typeof nome); // TIPO
 
 //EXERCICIO 1
-let carro = 'Ferrari'
-console.log(carro)
+let carro = 'Ferrari';
+console.log(carro);
 
-let precoBolo = 19.99
-console.log(precoBolo)
+let precoBolo = 19.99;
+console.log(precoBolo);
 
-let cidade = 'São Paulo'
+let cidade = 'São Paulo';
 
 //OPERAÇÕES EM VARIÁVEIS
-let x = 1
-x = x + 5
-let name = 'João'
-let sobrenome = 'Gonçalves'
-let nomeCompleto = name + ' ' + sobrenome
-console.log(x)
-console.log(nomeCompleto)
+let x = 1;
+x = x + 5;
+let name = 'João';
+let sobrenome = 'Gonçalves';
+let nomes = `${name} ${sobrenome}`;
+console.log(nomes);
+let nomeCompleto = name + ' ' + sobrenome;
+console.log(x);
+console.log(nomeCompleto);
 
+//CONDICIONAIS
+
+let age = 17;
+
+if (age > 18){
+    console.log('maior de idade');
+} else {
+    console.log('menor de idade');
+}
 
