92d9e9a553fe0db58f508e752ee367d546a74ff2
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2023-01-03 22:08:31 -0300

    ternario

--- script.js
+++ script.js
@@ -1,55 +1,60 @@
-let nome = 'Joao';
-let idade = 19;
-let logado = true;
-let lista = ['Joao', 19];
-let obj = {nome: 'joão', idade: 19 };
-console.log(typeof nome); // TIPO
+// let nome = 'Joao';
+// let idade = 19;
+// let logado = true;
+// let lista = ['Joao', 19];
+// let obj = {nome: 'joão', idade: 19 };
+// console.log(typeof nome); // TIPO
 
-//EXERCICIO 1
-let carro = 'Ferrari';
-console.log(carro);
+// //EXERCICIO 1
+// let carro = 'Ferrari';
+// console.log(carro);
 
-let precoBolo = 19.99;
-console.log(precoBolo);
+// let precoBolo = 19.99;
+// console.log(precoBolo);
 
-let cidade = 'São Paulo';
+// let cidade = 'São Paulo';
 
-//OPERAÇÕES EM VARIÁVEIS
-let x = 1;
-x = x + 5;
-let name = 'João';
-let sobrenome = 'Gonçalves';
-let nomes = `${name} ${sobrenome}`;
-console.log(nomes);
-let nomeCompleto = name + ' ' + sobrenome;
-console.log(x);
-console.log(nomeCompleto);
+// //OPERAÇÕES EM VARIÁVEIS
+// let x = 1;
+// x = x + 5;
+// let name = 'João';
+// let sobrenome = 'Gonçalves';
+// let nomes = `${name} ${sobrenome}`;
+// console.log(nomes);
+// let nomeCompleto = name + ' ' + sobrenome;
+// console.log(x);
+// console.log(nomeCompleto);
 
-//CONDICIONAIS
-let age = 17;
+// //CONDICIONAIS
+// let age = 17;
 
-if (age > 18){
-    console.log('maior de idade');
-} else {
-    console.log('menor de idade');
-}
+// if (age > 18){
+//     console.log('maior de idade');
+// } else {
+//     console.log('menor de idade');
+// }
 
-//EXERCICOS2
-let a = 10;
-let b = 5;
-console.log(a > b);
+// //EXERCICOS2
+// let a = 10;
+// let b = 5;
+// console.log(a > b);
 
-let w = '10';
-let z = 10;
-console.log( w === z);
+// let w = '10';
+// let z = 10;
+// console.log( w === z);
 
-let c = 10;
-let d = 5;
-console.log(c != d)
+// let c = 10;
+// let d = 5;
+// console.log(c != d)
 
-let preco = 40.3
-if(preco <= 45){
-    console.log('esta barato')
-} else {
-    console.log('esta caro')
-}
+// let preco = 40.3
+// if(preco <= 45){
+//     console.log('esta barato')
+// } else {
+//     console.log('esta caro')
+// }
+
+//TERNÁRIO
+let result = conditional ? true : false
+
+//SWITCH
