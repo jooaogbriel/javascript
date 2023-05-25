b2e7d8c61d683b13afcd726a0c87228467e4eb4e
João Gonçalves <joao_i0807@outlook.com>
2023-05-24 21:50:18 -0300

    work with objects

--- script.js
+++ script.js
@@ -328,148 +328,169 @@
 
 // console.log(r)
 
-const carros = [
-  {
-      modelo: "Ka",
-      marca: "Ford",
-      ano: "2000",
-      cor: "Branco",
-      completo: false,
-      acessorios: ['Vidro Elétrico'],
-      preco: 6799.33
-  },
-  {
-      modelo: "Gol",
-      marca: "VW",
-      ano: "1996",
-      cor: "Preto",
-      completo: false,
-      acessorios: ['Trava'],
-      preco: 12199.13
-  },
-  {
-      modelo: "Palio",
-      marca: "Fiat",
-      ano: "1995",
-      cor: "Verde",
-      completo: false,
-      acessorios: [],
-      preco: 11099.1
-  },
-  {
-      modelo: "Monza",
-      marca: "Chevrolet",
-      ano: "1993",
-      cor: "Vinho",
-      completo: false,
-      acessorios: [],
-      preco: 14578.25
-  },
-  {
-      modelo: "Saveiro",
-      marca: "VW",
-      ano: "2013",
-      cor: "Prata",
-      completo: false,
-      acessorios: [],
-      preco: 28399.13
-  },
-  {
-      modelo: "Gol",
-      marca: "VW",
-      ano: "1996",
-      cor: "Preto",
-      completo: true,
-      acessorios: ['Alarme', 'Trava', 'Ar', 'Vidro Elétrico'],
-      preco: 14350.45
-  },
-  {
-      modelo: "Gol",
-      marca: "VW",
-      ano: "2013",
-      cor: "Preto",
-      completo: true,
-      acessorios: ['Alarme', 'Trava', 'Ar', 'Vidro Elétrico'],
-      preco: 22109.21
-  },
-  {
-      modelo: "Montana",
-      marca: "Chevrolet",
-      ano: "2011",
-      cor: "Azul",
-      completo: false,
-      acessorios: [],
-      preco: 15999.13
-  },
-  {
-      modelo: "Stilo",
-      marca: "Fiat",
-      ano: "2000",
-      cor: "Preto",
-      completo: false,
-      acessorios: [],
-      preco: 17251.36
-  },
-  {
-    modelo: "F8 Spider",
-    marca: "Ferrari",
-    ano: "2024",
-    cor: "Vermelha",
-    completo: true,
-    acessorios: [],
-    preco: 4000000
-}
-]
+// const carros = [
+//   {
+//       modelo: "Ka",
+//       marca: "Ford",
+//       ano: "2000",
+//       cor: "Branco",
+//       completo: false,
+//       acessorios: ['Vidro Elétrico'],
+//       preco: 6799.33
+//   },
+//   {
+//       modelo: "Gol",
+//       marca: "VW",
+//       ano: "1996",
+//       cor: "Preto",
+//       completo: false,
+//       acessorios: ['Trava'],
+//       preco: 12199.13
+//   },
+//   {
+//       modelo: "Palio",
+//       marca: "Fiat",
+//       ano: "1995",
+//       cor: "Verde",
+//       completo: false,
+//       acessorios: [],
+//       preco: 11099.1
+//   },
+//   {
+//       modelo: "Monza",
+//       marca: "Chevrolet",
+//       ano: "1993",
+//       cor: "Vinho",
+//       completo: false,
+//       acessorios: [],
+//       preco: 14578.25
+//   },
+//   {
+//       modelo: "Saveiro",
+//       marca: "VW",
+//       ano: "2013",
+//       cor: "Prata",
+//       completo: false,
+//       acessorios: [],
+//       preco: 28399.13
+//   },
+//   {
+//       modelo: "Gol",
+//       marca: "VW",
+//       ano: "1996",
+//       cor: "Preto",
+//       completo: true,
+//       acessorios: ['Alarme', 'Trava', 'Ar', 'Vidro Elétrico'],
+//       preco: 14350.45
+//   },
+//   {
+//       modelo: "Gol",
+//       marca: "VW",
+//       ano: "2013",
+//       cor: "Preto",
+//       completo: true,
+//       acessorios: ['Alarme', 'Trava', 'Ar', 'Vidro Elétrico'],
+//       preco: 22109.21
+//   },
+//   {
+//       modelo: "Montana",
+//       marca: "Chevrolet",
+//       ano: "2011",
+//       cor: "Azul",
+//       completo: false,
+//       acessorios: [],
+//       preco: 15999.13
+//   },
+//   {
+//       modelo: "Stilo",
+//       marca: "Fiat",
+//       ano: "2000",
+//       cor: "Preto",
+//       completo: false,
+//       acessorios: [],
+//       preco: 17251.36
+//   },
+//   {
+//     modelo: "F8 Spider",
+//     marca: "Ferrari",
+//     ano: "2024",
+//     cor: "Vermelha",
+//     completo: true,
+//     acessorios: [],
+//     preco: 4000000
+// }
+// ]
 
-function contaTotal() {
-  return carros.length
-}
+// function contaTotal() {
+//   return carros.length
+// }
 
-function precoTotal(carros) {
-  let soma = 0
-  for (let i = 0; i < carros.length; i++){
-      soma += carros[i].preco
+// function precoTotal(carros) {
+//   let soma = 0
+//   for (let i = 0; i < carros.length; i++){
+//       soma += carros[i].preco
 
-  }
-  return `A soma do preço de todos veículos é R${soma}`
-}
+//   }
+//   return `A soma do preço de todos veículos é R${soma}`
+// }
 
-function filtraPorMarca(carros) {
-  var filtrado = carros.filter( carros => carros.completo === true);
-  return filtrado
+// function filtraPorMarca(carros) {
+//   var filtrado = carros.filter( carros => carros.completo === true);
+//   return filtrado
  
-}
+// }
 
-function adicionaCarro() {
-  return carros.push({
-    modelo: "F8 Spider",
-    marca: "Ferrari",
-    ano: "2024",
-    cor: "Vermelha",
-    completo: true,
-    acessorios: [],
-    preco: 4000000
-})
-}
+// function adicionaCarro() {
+//   return carros.push({
+//     modelo: "F8 Spider",
+//     marca: "Ferrari",
+//     ano: "2024",
+//     cor: "Vermelha",
+//     completo: true,
+//     acessorios: [],
+//     preco: 4000000
+// })
+// }
 
-function removeCarro(carros, index) {
-  carros.splice(index, 1)
-}
+// function removeCarro(carros, index) {
+//   carros.splice(index, 1)
+// }
 
-function contaCarrosNovos(carros) {
-  var filtrado = carros.filter( carros => carros.ano > 2013);
-  return filtrado
-}
+// function contaCarrosNovos(carros) {
+//   var filtrado = carros.filter( carros => carros.ano > 2013);
+//   return filtrado
+// }
 
-function adicionarDono(carros, indice, pessoa) {
-  carros[indice].donos = {
-    nome: pessoa.nome,
-    telefone: pessoa.telefone
-  };
+// function adicionarDono(carros, indice, pessoa) {
+//   carros[indice].donos = {
+//     nome: pessoa.nome,
+//     telefone: pessoa.telefone
+//   };
   
-  return carros;
-}
+//   return carros;
+// }
 
-r = adicionarDono(carros,1,{nome:'joao', telefone: 123})
-console.log(r)
-console.log(carros)
+// r = adicionarDono(carros,1,{nome:'joao', telefone: 123})
+// console.log(r)
+// console.log(carros)
+
+// const pessoa = {
+//   nome: 'João',
+//   anoDeNascimento: 1990,
+//   cidade: 'São Paulo'
+// };
+
+// function criarPessoa (nome, anoDeNascimento,cidade) {
+//   let pessoa = {
+//     nome : nome,
+//     anoDeNascimento: anoDeNascimento,
+//     cidade: cidade
+//   }
+// }
+
+// function fraseInitial (nome, anoDeNascimento,cidade) {
+//   let age = 2023 - anoDeNascimento
+//   return `${nome} possui ${age} anos de idade e atualmente está morando em ${cidade}.`
+// }
+// res = fraseInitial('joao',  2003, 'srpq') 
+// console.log(res)
